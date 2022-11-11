559db64bb94dd4a3bff6fd36c60f0b14a7ae9af9
Marijn Huizendveld <marijn@huizendveld.com>
2022-11-11 16:49:55 +0100

    :mag: `PricingEngineTest.calculatePrice_charged_per_minute`

---
+++ pricing-engine/PricingEngineTest.cs
@@ -0,0 +1,35 @@
+using NodaMoney;
+
+namespace pricing_engine;
+
+public class PricingEngineTest
+{
+    [Fact]
+    public void calculatePrice_charged_per_minute()
+    {
+        var pricingEngine = new PricingEngine();
+
+        var duration = Duration.ofMinutes(1);
+        var pricePerMinute = Money.Euro(0.30);
+        var actual = pricingEngine.calculatePrice(duration, pricePerMinute);
+
+        var expected = pricePerMinute;
+        Assert.Equal(expected, actual);
+    }
+}
+
+public class Duration
+{
+    public static Duration ofMinutes(int minutes)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class PricingEngine
+{
+    public Money calculatePrice(Duration duration, Money pricePerMinute)
+    {
+        throw new NotImplementedException();
+    }
+}
